46c569ca9d0e1e3ce63598a0420bdf88851f7113
RishDog001 <91677469+RishDog001@users.noreply.github.com>
2024-01-17 16:33:05 -0500

    Added project files
    Added the initial files for the project

---
+++ MoneyMountain/MoneyMountain/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyMountain
+{
+    public class Question
+    {
+        public string Text {  get; }
+        public List<string> Options { get; }
+        public int CorrectOption { get; }
+
+        public Question(string text, List<string> options, int correctOption)
+        {
+            Text = text;
+            Options = options;
+            CorrectOption = correctOption;
+        }
+    }
+}
